61c080e1392924b8cf3d7e0bf7537e7a9cd81e93
thenotsoft <thenotsoft@gmail.com>
2023-12-01 10:57:00 +0200

    wip

--- src/Collector/Web/WebAppInfoCollector.php
+++ src/Collector/Web/WebAppInfoCollector.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 use TTM\Telemetry\Collector\Collector\CollectorTrait;
-use TTM\Telemetry\ContextExtractorInterface;
+use TTM\Telemetry\Context\ContextExtractorInterface;
 use TTM\Telemetry\SpanInterface;
 use TTM\Telemetry\TracerInterface;
 use Yiisoft\ErrorHandler\Event\ApplicationError;
